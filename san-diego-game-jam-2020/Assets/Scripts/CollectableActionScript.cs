a811cdd551dd452eecfdad1bcd1fac9845bf78e9
Jon Apostol <japos003@ucr.edu>
2020-08-08 09:47:32 -0700

    Created CollectableBehavior for collectable itself, and CollectableAction for player, which determines if the player has invincibility or not

---
+++ san-diego-game-jam-2020/Assets/Scripts/CollectableActionScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableActionScript : MonoBehaviour
+{
+    public float _timeLimit;
+    public float _currentTime;
+    
+    public CrabStatus CurrentCrabStatus;
+
+    private Rigidbody _rigidbody;
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_currentTime > 0 && CurrentCrabStatus == CrabStatus.Invincible)
+        {
+            _currentTime -= Time.deltaTime;
+        }
+        else
+        {
+            CurrentCrabStatus = CrabStatus.Normal;
+            _currentTime = 0f;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Collectable")
+        {
+            CurrentCrabStatus = CrabStatus.Invincible;
+            _currentTime = _timeLimit;
+        }
+    }
+
+    public enum CrabStatus
+    {
+        Normal,
+        Invincible
+    }
+}
---
+++ san-diego-game-jam-2020/Assets/Scripts/CollectableBehaviorScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableBehaviorScript : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private Renderer _renderer;
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _renderer = GetComponent<Renderer>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TODO: Collectable animation script goes here?
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" && this.enabled)
+        {
+            this.enabled = false;
+            _renderer.enabled = false;
+            _rigidbody.detectCollisions = false;
+            _rigidbody.isKinematic = false;
+        }
+    }
+}
