c9ce2273558574f4802dcff1bc557531f616ed92
Jon Apostol <japos003@ucr.edu>
2020-08-07 23:56:25 -0700

    Added ability to jump. multiple jump still possible so might be fixed in a future commit

--- san-diego-game-jam-2020/Assets/Scripts/CrabMovementScript.cs
+++ san-diego-game-jam-2020/Assets/Scripts/CrabMovementScript.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class CrabMovementScript : MonoBehaviour
 {
-    public float _movementDirection = 1.0f;
+    public float _movementDirection;
+    public float _jumpForce;
+
+    private Rigidbody _rigidbody;
 
     void Awake()
     {
-
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        if(_movementDirection == 0)
+        {
+            _movementDirection = 1.0f;
+        }
 
+        if (_jumpForce == 0f) { _jumpForce = 250f; }
     }
 
     // Update is called once per frame
@@ -22,5 +30,10 @@ public class CrabMovementScript : MonoBehaviour
     {
         float movement = _movementDirection * Time.deltaTime;
         transform.position += new Vector3(movement, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+              _rigidbody.AddForce(new Vector3(0, _jumpForce, 0));
+        }
     }
 }
