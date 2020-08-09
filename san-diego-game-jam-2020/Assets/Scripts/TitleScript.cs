db5dc35ad074fe7b2713b9ed6cd543b8c64a1262
Jon Apostol <japos003@ucr.edu>
2020-08-09 00:34:55 -0700

    Adding title screen

---
+++ san-diego-game-jam-2020/Assets/Scripts/TitleScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TitleScript : MonoBehaviour
+{
+    public void GoToGameplay()
+    {
+        SceneManager.LoadScene("SceneName");
+     }
+}
